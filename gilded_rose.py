d45d12a8dfe56625e66047d7fae2c265329b070c
jiayangliu <liujiayang_ljy@163.com>
2024-10-08 17:21:33 -0700

    refactor with strategy pattern

--- gilded_rose.py
+++ gilded_rose.py
@@ -17,33 +17,70 @@ class GildedRose(object):
     def __init__(self, items: list[Item]):
         # DO NOT CHANGE THIS ATTRIBUTE!!!
         self.items = items
+        self.base_updator = BaseQualityUpdator()
+        self.updator_dict = {
+            "Aged Brie": AgedBrieUpdator(),
+            "Sulfuras, Hand of Ragnaros": SulfurasUpdator(),
+            "Backstage passes to a TAFKAL80ETC concert": BackstagePassesUpdator(),
+            "Conjured": ConjuredUpdator(),
+        }
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+            self.updator_dict.get(item.name, self.base_updator).update_quality(item)
+            self.check_quality(item)
+
+    def check_quality(self, item: Item):
+        '''check if 0 < item.quality < 50'''
+        if item.quality < 0:
+            item.quality = 0
+        if item.quality > 50:
+            item.quality = 50
+
+class BaseQualityUpdator:
+
+    def update_quality(self, item: Item):
+        '''base method to update item quality'''
+        item.quality -= 1
+        item.sell_in -= 1
+        if item.sell_in < 0:
+            item.quality -= 1
+
+
+class AgedBrieUpdator(BaseQualityUpdator):
+    '''Aged Brie actually increases in Quality the older it gets'''
+
+    def update_quality(self, item: Item):
+        item.sell_in -= 1
+        item.quality += 1
+
+
+class SulfurasUpdator(BaseQualityUpdator):
+    '''Sulfuras, being a legendary item, never has to be sold or decreases in Quality'''
+
+    def update_quality(self, item: Item):
+        pass
+
+class BackstagePassesUpdator(BaseQualityUpdator):
+    '''Backstage passes, like aged brie, increases in Quality as its SellIn value approaches;
+    Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
+    Quality drops to 0 after the concert'''
+
+    def update_quality(self, item: Item):
+        if item.sell_in > 0:
+            item.quality += 1
+            if item.sell_in < 11:
+                    item.quality += 1
+            if item.sell_in < 6:
+                    item.quality += 1
+        else:
+            item.quality = 0
+        item.sell_in -= 1
+
+
+class ConjuredUpdator(BaseQualityUpdator):
+    '''Conjured items degrade in Quality twice as fast as normal items'''
+
+    def update_quality(self, item: Item):
+        item.quality -= 2
+        item.sell_in -= 1
--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -9,33 +9,35 @@ class GildedRoseTest(unittest.TestCase):
         items = [Item("foo", 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        self.assertEqual("foo", items[0].name)
+
 
     def test_quality_not_negative(self):
         '''The Quality of an item is never negative'''
         item_name = "negative_quality"
-        items = [Item(item_name, 1, 2), Item(item_name, 1, 1), Item(item_name, 1, 0), ]
+        items = [Item(item_name, 1, 2), Item(item_name, 1, 0), ]
         gr = GildedRose(items)
 
         gr.update_quality()
 
-        assert gr.items == [Item(item_name, 0, 1), Item(item_name, 0, 0), Item(item_name, 0, 0)]
+        assert gr.items[0].quality == 1 and gr.items[1].quality == 0
 
 
     def test_quality_less_than_50(self):
         '''The Quality of an item is never more than 50'''
         item_name = "expensive_item"
-        items = [Item(item_name, 1, 2), Item(item_name, 9, 19), Item(item_name, 4, 55), ]
+        items = [Item(item_name, 1, 2), Item(item_name, 1, 55), ]
         gr = GildedRose(items)
 
         gr.update_quality()
 
-        assert gr.items == [Item(item_name, 0, 1), Item(item_name, 8, 18)]
+        assert gr.items[0].quality == 1 and gr.items[1].quality == 50
+
 
     def test_aged_brice(self):
         ''' "Aged Brie" actually increases in Quality the older it gets'''
         init_quality = 20
-        items = [Item("NOT Aged Brie", 10, init_quality)]
+        items = [Item("Aged Brie", 10, init_quality)]
         gr = GildedRose(items)
 
         gr.update_quality()
